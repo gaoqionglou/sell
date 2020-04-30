7e9110d4512c9960e1d157d2bfc265202dd75480
gaoqionglou <gaoqionglou@csair.com>
2020-04-30 17:05:33 +0800

    vue app learning

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,10 +1,338 @@
 <template>
-  <div>Ratings</div>
+  <div class="ratings" ref="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+      <split></split>
+      <ratingselect
+        :select-type="selectType"
+        :only-content="onlyContent"
+        :desc="desc"
+        :ratings="ratings"
+        @select="selectRating"
+        @toggle="toggleContent"
+      ></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li
+            class="rating-item border-1px"
+            v-for="(rating,index) in ratings"
+            v-show="needShow(rating.rateType, rating.text)"
+            :key="index"
+          >
+            <div class="avatar">
+              <img width="28" height="28" :scr="rating.avatar" />
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="(item,i) in rating.recommend" :key="i">{{item}}</span>
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  /* eslint-disable */
+/* eslint-disable */
+import star from "../../components/star/star";
+import split from "../split/split";
+import ratingselect from "../ratingselect/ratingselect";
+import { formatDate } from "../../common/js/date";
+import BScroll from "better-scroll";
+const ERR_OK = 0;
+const POSITIVE = 0;
+const NEGATIVE = 1;
+const ALL = 2;
+export default {
+  components: { star, split, ratingselect },
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      showFlag: false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        all: "全部",
+        positive: "推荐",
+        negative: "吐槽"
+      },
+      ratings: []
+    };
+  },
+  created() {
+    this.$http.get("/api/ratings").then(
+      res => {
+        if (res.body.errno === ERR_OK) {
+          this.ratings = res.body.data;
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$refs.ratings, {
+              click: true
+            });
+          });
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  },
+  methods: {
+    selectRating(type) {
+      this.selectType = type;
+      this.$nextTick(() => {
+        this.scroll.refresh();
+      });
+    },
+    toggleContent(onlyContent) {
+      this.onlyContent = !onlyContent;
+      this.$nextTick(() => {
+        this.scroll.refresh();
+      });
+    },
+    needShow(rateType, rateText) {
+      if (this.onlyContent && !rateText) {
+        return false;
+      }
+      if (this.selectType == ALL) {
+        return true;
+      } else {
+        return rateType === this.selectType;
+      }
+    }
+  },
+  filters: {
+    formatDate(time) {
+      let date = new Date(time);
+      return formatDate(date, "yyyy-MM-dd hh:mm");
+    }
+  }
+};
 </script>
 
 <style lang="stylus">
+@import '../../common/stylus/mixin.styl';
+
+.ratings {
+  position: absolute;
+  top: 174px;
+  bottom: 0;
+  width: 100%;
+  overflow: hidden;
+
+  .overview {
+    display: flex;
+    padding: 18px 0;
+    text-align: center;
+
+    .overview-left {
+      flex: 0 0 137px;
+      width: 137px;
+      padding: 6px 0;
+      border-right: 1px solid rgba(7, 17, 27, 0.2);
+
+      @media only screen and (max-width: 320px) {
+        flex: 0 0 120px;
+        width: 120px;
+      }
+
+      .score {
+        margin-bottom: 6px;
+        line-height: 28px;
+        font-size: 24px;
+        color: rgb(255, 153, 0);
+      }
+
+      .title {
+        margin-bottom: 8px;
+        line-height: 12px;
+        font-size: 12px;
+        color: rgb(7, 17, 27);
+      }
+
+      .rank {
+        line-height: 10px;
+        font-size: 10px;
+        color: rgb(147, 153, 159);
+      }
+    }
+
+    .overview-right {
+      flex: 1;
+      padding: 6px 0 6px 24px;
+      text-align: left;
+
+      @media only screen and (max-width: 320px) {
+        padding-left: 6px;
+      }
+
+      .score-wrapper {
+        margin-bottom: 8px;
+        font-size: 0;
+
+        .title {
+          display: inline-block;
+          line-height: 18px;
+          vertical-align: top;
+          font-size: 12px;
+          color: rgb(7, 17, 27);
+        }
+
+        .star {
+          display: inline-block;
+          margin: 0 12px;
+          vertical-align: top;
+        }
+
+        .score {
+          display: inline-block;
+          line-height: 18px;
+          vertical-align: top;
+          font-size: 12px;
+          color: rgb(255, 153, 0);
+        }
+      }
+    }
+
+    .delivery-wrapper {
+      font-size: 0;
+
+      .title {
+        line-height: 18px;
+        font-size: 12px;
+        color: rgb(7, 17, 27);
+      }
+
+      .delivery {
+        margin-left: 12px;
+        font-size: 12px;
+        color: rgb(147, 153, 159);
+      }
+    }
+  }
+
+  .rating-wrapper {
+    padding: 0 18px;
+
+    .rating-item {
+      display: flex;
+      padding: 18px 0;
+      border-1px(rgb(7, 17, 27));
+
+      .avatar {
+        flex: 0 0 28px;
+        width: 28px;
+        margin-right: 12px;
+
+        img {
+          border-radius: 50%;
+        }
+      }
+
+      .content {
+        flex: 1;
+        position: relative;
+
+        .name {
+          margin-bottom: 4px;
+          line-height: 12px;
+          font-size: 10px;
+          color: rgb(7, 17, 27);
+        }
+
+        .star-wrapper {
+          margin-bottom: 6px;
+          font-size: 0;
+
+          .star {
+            display: inline-block;
+            margin-left: 6px;
+            vertical-align: top;
+          }
+
+          .delivery {
+            display: inline-block;
+            vertical-align: top;
+            line-height: 12px;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+        }
+
+        .text {
+          margin-bottom: 8px;
+          line-height: 18px;
+          color: rgb(7, 17, 27);
+        }
+
+        .recommend {
+          line-height: 16px;
+          font-size: 0;
+
+          .icon-thumb_up, .item {
+            display: inline-block;
+            margin: 0 8px 4px 0;
+            font-size: 9px;
+          }
+
+          .icon-thumb_up {
+            color: rgb(0, 160, 220);
+          }
+
+          .item {
+            padding: 0 6px;
+            border: 1px solid rgba(7, 17, 27, 0.1);
+            border-radius: 1px;
+            color: rgb(147, 153, 159);
+            background: #fff;
+          }
+        }
+      }
+
+      .time {
+        position: absolute;
+        top: 0;
+        right: 0;
+        line-height: 12px;
+        font-size: 10px;
+        color: rgb(147, 153, 159);
+      }
+    }
+  }
+}
 </style>
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,10 +1,69 @@
 <template>
-  <div>Seller</div>
+  <div class="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title"></h1>
+        <star :size="36" :score="seller.score"></star>
+        <span class="text">({{seller.ratingCount}})</span>
+        <span class="text">{{seller.sellerCount}}</span>
+      </div>
+      <ul class="remark">
+        <li class="block">
+          <h2>起送价</h2>
+          <div class="content">
+            <span class="stress">{{seller.minPrice}}</span>元
+          </div>
+        </li>
+        <li class="block">
+          <h2>商家配送</h2>
+          <div class="content">
+            <span class="stress">{{seller.deliveryPrice}}</span>元
+          </div>
+        </li>
+        <li class="block">
+          <h2>平均配送时间</h2>
+          <div class="content">
+            <span class="stress">{{seller.deliveryTime}}</span>元
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  /* eslint-disable */
+/* eslint-disable */
+import star from "../../components/star/star";
+export default {
+  components: { star },
+  props: {
+    seller: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  }
+};
 </script>
 
 <style lang="stylus">
+@import '../../common/stylus/mixin.styl';
+
+.seller {
+  position: absolute;
+  top: 174px;
+  bottom: 0;
+  width: 100%;
+  overflow: hidden;
+
+  .overview {
+    // position relative
+    padding: 18px;
+    .title{
+      margin-bottom: 8px
+      
+    }
+  }
+}
 </style>
